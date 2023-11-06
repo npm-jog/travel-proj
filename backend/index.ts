3a87bdd7ab85678798c9a91de1be602fd8172779
Dean <dean96@live.co.uk>
2023-11-06 10:47:18 +0000

    setup typescript and express

---
+++ backend/index.ts
@@ -0,0 +1,7 @@
+//import express, {Express, Request, Response, Application} from 'express';
+
+const express = require('express');
+const {Application} = require ('express');
+
+
+const app = express();
