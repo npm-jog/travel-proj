e22866ad3b21839c6bfe75a16389e4e4cf5e5cca
Arron-B <arronmail@yahoo.com>
2023-11-07 15:23:33 +0000

    reviews controller, model and routes setup. Waiting for DB to test.

---
+++ backend/models/reviews.models.ts
@@ -0,0 +1,34 @@
+function fetchReviewsByLocation() {
+	const myPromise: Promise<string> = new Promise((resolve, reject) => {
+		resolve("hello" + "world");
+	});
+	return myPromise;
+}
+
+function addReviewToDb() {
+	const myPromise: Promise<string> = new Promise((resolve, reject) => {
+		resolve("hello" + "world");
+	});
+	return myPromise;
+}
+
+function removeReviewFromDb() {
+	const myPromise: Promise<string> = new Promise((resolve, reject) => {
+		resolve("hello" + "world");
+	});
+	return myPromise;
+}
+
+function editReviewInDb() {
+	const myPromise: Promise<string> = new Promise((resolve, reject) => {
+		resolve("hello" + "world");
+	});
+	return myPromise;
+}
+
+export {
+	fetchReviewsByLocation,
+	addReviewToDb,
+	removeReviewFromDb,
+	editReviewInDb,
+};
