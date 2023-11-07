93ef670bca36eea5c09bc1e287e1b4236e7592c9
Jeremy Merlin <jeremymerlin@hotmail.fr>
2023-11-07 09:19:00 +0000

    update folder name

---
+++ Database/db/data/test-data/index.js
@@ -0,0 +1,5 @@
+exports.commentData = require("./comments.js");
+exports.questionData = require("./questions.js");
+exports.reviewData = require("./reviews.js");
+exports.userData = require("./users.js");
+exports.countryData = require("./country.js");
