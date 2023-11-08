6dfe58b7a68fa38fc653d2032477985d1a0fc84c
Arron-B <arronmail@yahoo.com>
2023-11-08 10:31:05 +0000

    navbar button bug fixed. buttons and icons added to navbar

--- client/TravelApp/src/App.vue
+++ client/TravelApp/src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <ion-app>
-    <side-menu />
-    <nav-bar />
-    <ion-router-outlet />
-  </ion-app>
+	<ion-app>
+		<side-menu />
+		<nav-bar />
+		<ion-router-outlet id="main-content" />
+	</ion-app>
 </template>
 
 <script setup lang="ts">
-import { IonApp, IonRouterOutlet } from "@ionic/vue";
+	import { IonApp, IonRouterOutlet } from "@ionic/vue";
 </script>
--- client/TravelApp/src/components/SideMenu.vue
+++ client/TravelApp/src/components/SideMenu.vue
@@ -1,34 +1,130 @@
 <template>
-  <ion-menu content-id="main-content" side="start">
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Menu Content</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content class="ion-padding">This is the menu content.</ion-content>
-  </ion-menu>
+	<ion-menu
+		content-id="main-content"
+		side="start"
+	>
+		<ion-header>
+			<ion-toolbar>
+				<ion-title>Menu</ion-title>
+			</ion-toolbar>
+		</ion-header>
+		<ion-content class="ion-padding">
+			<ion-button expand="block">
+				Home
+				<ion-icon
+					slot="end"
+					:icon="home"
+				></ion-icon>
+			</ion-button>
+			<ion-button expand="block">
+				Profile
+				<ion-icon
+					slot="end"
+					:icon="personOutline"
+				></ion-icon>
+			</ion-button>
+			<ion-button expand="block">
+				Create Account
+				<ion-icon
+					slot="end"
+					:icon="personAddOutline"
+				></ion-icon>
+			</ion-button>
+			<ion-button expand="block">
+				Sign In
+				<ion-icon
+					slot="end"
+					:icon="logInOutline"
+				></ion-icon>
+			</ion-button>
+			<ion-button expand="block">
+				Sign Out
+				<ion-icon
+					slot="end"
+					:icon="logOutOutline"
+				></ion-icon>
+			</ion-button>
+			<ion-button expand="block">
+				Gallery
+				<ion-icon
+					slot="end"
+					:icon="imageOutline"
+				></ion-icon>
+			</ion-button>
+			<ion-button expand="block">
+				My Reviews
+				<ion-icon
+					slot="end"
+					:icon="starHalfOutline"
+				></ion-icon>
+			</ion-button>
+			<ion-button expand="block">
+				Conversations
+				<ion-icon
+					slot="end"
+					:icon="chatboxEllipsesOutline"
+				></ion-icon>
+			</ion-button>
+			<ion-button expand="block">
+				Wishlist
+				<ion-icon
+					slot="end"
+					:icon="giftOutline"
+				></ion-icon>
+			</ion-button>
+		</ion-content>
+	</ion-menu>
 </template>
 
 <script lang="ts">
-import {
-  IonContent,
-  IonHeader,
-  IonMenu,
-  IonPage,
-  IonTitle,
-  IonToolbar,
-} from "@ionic/vue";
+	import {
+		IonContent,
+		IonHeader,
+		IonMenu,
+		IonPage,
+		IonTitle,
+		IonToolbar,
+		IonButton,
+		IonIcon,
+	} from "@ionic/vue";
 
-import { defineComponent } from "vue";
+	import {
+		home,
+		personOutline,
+		personAddOutline,
+		logInOutline,
+		logOutOutline,
+		imageOutline,
+		starHalfOutline,
+		chatboxEllipsesOutline,
+		giftOutline,
+	} from "ionicons/icons";
 
-export default defineComponent({
-  components: {
-    IonContent,
-    IonHeader,
-    IonMenu,
-    IonPage,
-    IonTitle,
-    IonToolbar,
-  },
-});
+	import { defineComponent } from "vue";
+
+	export default defineComponent({
+		components: {
+			IonContent,
+			IonHeader,
+			IonMenu,
+			IonPage,
+			IonTitle,
+			IonToolbar,
+			IonButton,
+			IonIcon,
+		},
+		setup() {
+			return {
+				home,
+				personOutline,
+				personAddOutline,
+				logInOutline,
+				logOutOutline,
+				imageOutline,
+				starHalfOutline,
+				chatboxEllipsesOutline,
+				giftOutline,
+			};
+		},
+	});
 </script>
