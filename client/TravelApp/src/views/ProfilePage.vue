b8ce698ae4d4c17617802d079ef24c8f48d0efd2
LDEV95 <lawrencefanutzadavis@gmail.com>
2023-11-09 16:16:49 +0000

    basic layout of profile page complete

--- client/TravelApp/src/views/ProfilePage.vue
+++ client/TravelApp/src/views/ProfilePage.vue
@@ -2,10 +2,34 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Travel App</ion-title>
+        <ion-title class="ion-text-center">Your Profile</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content :fullscreen="true">Profile Page</ion-content>
+
+    <ion-content :fullscreen="true">
+      <div class="content-container">
+        <h1 class="welcomeHeader">Welcome to your profile!</h1>
+
+        <!-- Profile Photo Section -->
+        <div>
+          <img
+            class="profile_pic"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlbPhSvnw2J3HeWJ4SjZf5ca8esFf_7jmtUhEmY9tKs2Tp6H4bUX2Kitm6Zq--lG73cpE&usqp=CAU" />
+        </div>
+
+        <div class="profile-section">
+          <ion-title class="section-title">My Galleries</ion-title>
+        </div>
+
+        <div class="profile-section">
+          <ion-title class="section-title">Wishlist</ion-title>
+        </div>
+
+        <ion-button @click="handleEditProfileClick" class="editProfile"
+          >Edit profile</ion-button
+        >
+      </div>
+    </ion-content>
   </ion-page>
 </template>
 
@@ -16,5 +40,51 @@ import {
   IonToolbar,
   IonTitle,
   IonContent,
+  IonButton,
 } from "@ionic/vue";
+
+const handleEditProfileClick = () => {
+  console.log("Edit profile button clicked!");
+};
 </script>
+
+<style>
+ion-content {
+  text-align: center;
+  font-weight: bold;
+}
+
+ion-title {
+  margin-top: 0;
+}
+
+.content-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: start;
+  padding: 1.25em;
+}
+
+.profile-section {
+  margin-top: 3.75em;
+}
+
+.section-title {
+  font-size: 1.25em;
+  font-weight: bold;
+}
+
+.editProfile {
+  margin-top: 2em;
+  width: 12.5em;
+}
+
+.profile_pic {
+  margin-top: 2em;
+}
+
+.welcomeHeader {
+  font-size: 3.125em;
+}
+</style>
