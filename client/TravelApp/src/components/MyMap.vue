baab5811fa4a3b0807f96aae285d4050a219ad67
Laurentiu Patrascu <lpatrascu55@yahoo.com>
2023-11-10 11:39:33 +0000

    Map now centers on chosen country when pressing enter

--- client/TravelApp/src/components/MyMap.vue
+++ client/TravelApp/src/components/MyMap.vue
@@ -1,91 +1,104 @@
 <template>
-	<div>
-		<capacitor-google-map
-			ref="mapRef"
-			style="display: inline-block; width: 100vw; height: 100vh"
-		>
-		</capacitor-google-map>
-	</div>
+  <div>
+    <capacitor-google-map
+      ref="mapRef"
+      style="display: inline-block; width: 100vw; height: 100vh"
+      @keyup.enter="logCountries(this.filteredCountries[0])"
+    >
+    </capacitor-google-map>
+  </div>
 </template>
 
+<script lang="ts"></script>
+
 <script setup lang="ts">
-	import { onMounted, nextTick, ref, onUnmounted, defineComponent } from "vue";
-	import { GoogleMap } from "@capacitor/google-maps";
-	import apiKey from "@/components/APIKey.js";
+import { onMounted, nextTick, ref, onUnmounted, defineComponent } from "vue";
+import { GoogleMap } from "@capacitor/google-maps";
+import apiKey from "@/components/APIKey.js";
+import { toRaw } from "vue";
+let newCoordinates: any;
+let newZoom: any;
+async function logCountries(passedArg: any) {
+  if (passedArg) {
+    newCoordinates = toRaw(passedArg.coordinates);
+    newZoom = 6;
+    await newMap.removeAllMapListeners();
+    await newMap.destroy();
+    await createMap();
+  }
+}
+const props = defineProps<{
+  markerData: { coordinate: any; title: string; snippet: string }[];
+  filteredCountries: any;
+}>();
 
-	const props = defineProps<{
-		markerData: { coordinate: any; title: string; snippet: string }[];
-		filteredCountries: any;
-	}>();
+// EVENTS
+const emits = defineEmits<{
+  (event: "onMarkerClicked", info: any): void;
+  (event: "onMapClicked"): void;
+}>();
 
-	// EVENTS
-	const emits = defineEmits<{
-		(event: "onMarkerClicked", info: any): void;
-		(event: "onMapClicked"): void;
-	}>();
+const mapRef = ref<HTMLElement>();
+const markerIds = ref<string[] | undefined>();
+let newMap: GoogleMap;
 
-	const mapRef = ref<HTMLElement>();
-	const markerIds = ref<string[] | undefined>();
-	let newMap: GoogleMap;
+onMounted(async () => {
+  await nextTick();
+  await createMap();
+});
 
-	onMounted(async () => {
-		await nextTick();
-		await createMap();
-	});
+// remove markers on unmount
+onUnmounted(() => {
+  newMap.removeMarkers(markerIds?.value as string[]);
+});
 
-	// remove markers on unmount
-	onUnmounted(() => {
-		newMap.removeMarkers(markerIds?.value as string[]);
-	});
+const addSomeMarkers = async (newMap: GoogleMap) => {
+  markerIds?.value && newMap.removeMarkers(markerIds?.value as string[]);
 
-	const addSomeMarkers = async (newMap: GoogleMap) => {
-		markerIds?.value && newMap.removeMarkers(markerIds?.value as string[]);
+  // Plot each point on the map
+  let markers = props.markerData.map(({ coordinate, title, snippet }) => {
+    return {
+      coordinate,
+      title,
+      snippet,
+    };
+  });
 
-		// Plot each point on the map
-		let markers = props.markerData.map(({ coordinate, title, snippet }) => {
-			return {
-				coordinate,
-				title,
-				snippet,
-			};
-		});
+  markerIds.value = await newMap.addMarkers(markers);
+};
 
-		markerIds.value = await newMap.addMarkers(markers);
-	};
+/**
+ *
+ */
+async function createMap() {
+  if (!mapRef.value) return;
+  // render map using capacitor plugin
+  newMap = await GoogleMap.create({
+    id: "my-cool-map",
+    element: mapRef.value,
+    apiKey: apiKey.mapsKey,
+    config: {
+      center: newCoordinates || {
+        lat: 30,
 
-	/**
-	 *
-	 */
-	async function createMap() {
-		if (!mapRef.value) return;
+        lng: 0,
+      },
+      zoom: newZoom || 3,
+    },
+  });
 
-		// render map using capacitor plugin
-		newMap = await GoogleMap.create({
-			id: "my-cool-map",
-			element: mapRef.value,
-			apiKey: apiKey.mapsKey,
-			config: {
-				center: {
-					lat: 30,
+  // add markers to map
+  addSomeMarkers(newMap);
 
-					lng: 0,
-				},
-				zoom: 2.5,
-			},
-		});
+  // Set Event Listeners...
+  // Handle marker click, send event back to parent
+  newMap.setOnMarkerClickListener((event) => {
+    emits("onMarkerClicked", event);
+  });
 
-		// add markers to map
-		addSomeMarkers(newMap);
-
-		// Set Event Listeners...
-		// Handle marker click, send event back to parent
-		newMap.setOnMarkerClickListener((event) => {
-			emits("onMarkerClicked", event);
-		});
-
-		// Handle map click, send event back to parent
-		newMap.setOnMapClickListener(() => {
-			emits("onMapClicked");
-		});
-	}
+  // Handle map click, send event back to parent
+  newMap.setOnMapClickListener(() => {
+    emits("onMapClicked");
+  });
+}
 </script>
