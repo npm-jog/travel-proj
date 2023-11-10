e558f22afc0683ff95c36ecda9fb9e6c176e8bf4
Laurentiu Patrascu <lpatrascu55@yahoo.com>
2023-11-10 09:18:01 +0000

    Fixed Export errors

--- client/TravelApp/src/components/NavBar.vue
+++ client/TravelApp/src/components/NavBar.vue
@@ -1,22 +1,26 @@
 <template>
-	<ion-header>
-		<ion-toolbar class="select-toolbar">
-			<ion-buttons slot="start">
-				<ion-menu-button></ion-menu-button>
-			</ion-buttons>
-		</ion-toolbar>
-	</ion-header>
+  <ion-header>
+    <ion-toolbar class="select-toolbar">
+      <ion-buttons slot="start">
+        <ion-menu-button></ion-menu-button>
+      </ion-buttons>
+    </ion-toolbar>
+  </ion-header>
 </template>
 
 <script lang="ts">
-	import { IonButtons, IonMenuButton, IonToolbar, IonHeader } from "@ionic/vue";
+import { IonButtons, IonMenuButton, IonToolbar, IonHeader } from "@ionic/vue";
+import { defineComponent } from "vue";
+export default defineComponent({
+  components: { IonMenuButton, IonButtons, IonToolbar, IonHeader },
+});
 </script>
 
 <style scoped>
-	.select-toolbar {
-		position: absolute;
-		height: 3.5rem;
-		width: 15%;
-		float: left;
-	}
+.select-toolbar {
+  position: absolute;
+  height: 3.5rem;
+  width: 15%;
+  float: left;
+}
 </style>
