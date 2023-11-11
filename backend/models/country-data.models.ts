310e96651662c08cc6c1ae18874888c956bdc4a2
Jeremy Merlin <jeremymerlin@hotmail.fr>
2023-11-11 12:52:28 +0000

    improve weather, test, key in .env

--- backend/models/country-data.models.ts
+++ backend/models/country-data.models.ts
@@ -1,25 +1,23 @@
-import axios from 'axios';
-import {PublicHolidays, RequestError} from '../types/country-data.interfaces';
-
+import axios from "axios";
+import { PublicHolidays, RequestError, WeatherData } from "../types/country-data.interfaces";
 
 async function fetchPublicHolidays(year: string, countryCode: string) {
-    try {
-      const response: any = await axios.get(`https://date.nager.at/api/v3/PublicHolidays/${year}/${countryCode}`)
-      const holidays: PublicHolidays[] = response.data
-      if(!holidays) return Promise.reject({status: 400, msg: 'Invalid country code'})
-      return holidays;
-    } catch (err: any) {
-      const msg: string = err.response.data.status === 400 ? 'Bad query request' : 'Invalid country code';
-      const queryError: RequestError = {status: err.response.data.status, msg}
-      return Promise.reject(queryError)
-    }
+  try {
+    const response: any = await axios.get(`https://date.nager.at/api/v3/PublicHolidays/${year}/${countryCode}`);
+    const holidays: PublicHolidays[] = response.data;
+    if (!holidays) return Promise.reject({ status: 400, msg: "Invalid country code" });
+    return holidays;
+  } catch (err: any) {
+    const msg: string = err.response.data.status === 400 ? "Bad query request" : "Invalid country code";
+    const queryError: RequestError = { status: err.response.data.status, msg };
+    return Promise.reject(queryError);
   }
-
+}
 
 async function fetchWeatherData(apiUrl: string) {
   try {
-    const { data } = await axios.get(apiUrl);
-    const weather = {
+    const { data }: any = await axios.get(apiUrl);
+    const weather: WeatherData = {
       name: data.location.name,
       country: data.location.country,
       localtime: data.location.localtime,
@@ -30,6 +28,7 @@ async function fetchWeatherData(apiUrl: string) {
         precip_mm: data.current.precip_mm,
       },
     };
+    if (!weather) return Promise.reject({ status: 400, msg: "Invalid city name" });
     return weather;
   } catch (err) {
     return Promise.reject({ status: 400, msg: "unexpected error" });
--- backend/types/country-data.interfaces.ts
+++ backend/types/country-data.interfaces.ts
@@ -1,18 +1,30 @@
 export interface PublicHolidays {
-	date: string;
-	localName: string;
-	name: string;
-	countryCode: String;
-    fixed: Boolean;
-    global: Boolean;
-    countries: any;
-    launchYear: any;
-    types: String[];
+  date: string;
+  localName: string;
+  name: string;
+  countryCode: String;
+  fixed: Boolean;
+  global: Boolean;
+  countries: any;
+  launchYear: any;
+  types: String[];
 }
 
-export interface PublicHolidaysArray extends Array<PublicHolidays>{}
+export interface WeatherData {
+  name: string;
+  country: string;
+  localtime: string;
+  weather: {
+    temp_c: number;
+    condition: string;
+    icon: string;
+    precip_mm: number;
+  };
+}
+
+export interface PublicHolidaysArray extends Array<PublicHolidays> {}
 
 export interface RequestError {
-    status: number,
-    msg: string
+  status: number;
+  msg: string;
 }
