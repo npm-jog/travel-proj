81d823e518c5d54ce112139a44890e003119c32b
Arron-B <arronmail@yahoo.com>
2023-11-13 21:33:48 +0000

    Changed global store to keep all user info on one object
    Added username and clickable avatar to nav which directs to profile. removed profile button from nav
    
    Retrieved user info on all page components

--- client/TravelApp/src/views/Conversations.vue
+++ client/TravelApp/src/views/Conversations.vue
@@ -9,6 +9,23 @@
 	</ion-page>
 </template>
 
+<script lang="ts">
+	import { mapGetters } from "vuex";
+
+	export default defineComponent({
+		computed: {
+			// Use mapGetters to access the getUser getter from the store
+			...mapGetters(["getUserInfo"]),
+
+			// Use a computed property to get the user from the store
+			userInfo() {
+				return this.getUserInfo;
+			},
+		},
+	});
+</script>
+
 <script setup lang="ts">
 	import { IonPage, IonContent } from "@ionic/vue";
+	import { defineComponent } from "vue";
 </script>
--- client/TravelApp/src/views/WishlistPage.vue
+++ client/TravelApp/src/views/WishlistPage.vue
@@ -6,10 +6,9 @@
 			ion-padding
 		>
 			<div class="wishlist-section">
-				<h1 v-if="username">{{ username }} Wishlist</h1>
-				<h1 v-if="userId">{{ userId }} Wishlist</h1>
-				<h1 v-if="userEmail">{{ userEmail }} Wishlist</h1>
-				<!-- <h1 v-if="user">{{ user?.name }} Wishlist</h1> -->
+				<h1 v-if="userInfo">{{ userInfo.username }}'s Wishlist</h1>
+				<h1 v-if="userInfo">{{ userInfo.email }} Wishlist</h1>
+				<h1 v-if="userInfo">{{ userInfo.wishlist }} Wishlist</h1>
 			</div>
 		</ion-content>
 	</ion-page>
@@ -22,17 +21,11 @@
 		components: {},
 		computed: {
 			// Use mapGetters to access the getUser getter from the store
-			...mapGetters(["getUsername", "getUserEmail", "getUserId"]),
+			...mapGetters(["getUserInfo"]),
 
 			// Use a computed property to get the user from the store
-			username() {
-				return this.getUsername;
-			},
-			userId() {
-				return this.getUserId;
-			},
-			userEmail() {
-				return this.getUserEmail;
+			userInfo() {
+				return this.getUserInfo;
 			},
 		},
 	});
