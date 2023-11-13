e5dcf62393b9d7c39668ba1cb87a1a25634fc4e3
JamesLai10 <JamesLai2645@gmail.com>
2023-11-13 10:10:42 +0000

    started my galleries page

---
+++ client/TravelApp/src/views/MyGalleries.vue
@@ -0,0 +1,17 @@
+<template>
+	<ion-page>
+		<ion-content :fullscreen="true">
+			<h1 class="my-galleries-title">My Galleries</h1>
+		</ion-content>
+	</ion-page>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style>
+.my-galleries-title {
+    text-align: center;
+}
+</style>
