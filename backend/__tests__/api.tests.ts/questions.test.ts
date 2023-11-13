e6902e89a9697f91b580b4e128eb5709f7d72752
Dean <dean96@live.co.uk>
2023-11-13 11:00:20 +0000

    remove log

--- backend/__tests__/api.tests.ts/questions.test.ts
+++ backend/__tests__/api.tests.ts/questions.test.ts
@@ -28,7 +28,6 @@ describe("GET getQuestions", () => {
       .expect(200)
       .then(({ body }) => {
         const questions: QuestionType[] = body.questions;
-        console.log(questions)
         expect(questions).toHaveLength(6);
         questions.forEach((question: QuestionType) => {
           expect(question).toEqual(
