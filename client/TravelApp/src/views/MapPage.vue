e7adf369155e48b1517aaf3d0350e93883493161
Laurentiu Patrascu <lpatrascu55@yahoo.com>
2023-11-13 03:06:07 +0000

    Refactored MyMap and MapPage into 1 file and set up map centering on search bar

--- client/TravelApp/src/views/MapPage.vue
+++ client/TravelApp/src/views/MapPage.vue
@@ -6,43 +6,74 @@
         placeholder="Search for country"
         v-model="searchQuery"
         @ionInput="handleSearch(countriesArr)"
+        @keyup.enter="resetMap(searchResult[0])"
       ></ion-searchbar>
-      <my-map
-        class="map"
-        :markerData="markerData"
-        v-bind:filteredCountries="filteredCountries"
-        @onMapClicked="mapClicked"
-        @onMarkerClicked="markerClicked"
-      ></my-map>
-      <ion-popover
-        :is-open="markerIsOpen"
-        size="cover"
-        @did-dismiss="markerIsOpen = false"
-      >
-        <ion-content class="ion-padding">
-          <div>{{ markerInfo?.title }}</div>
-        </ion-content>
-      </ion-popover>
+      <div>
+        <capacitor-google-map
+          ref="mapRef"
+          style="display: inline-block; width: 100vw; height: 100vh"
+        >
+        </capacitor-google-map>
+      </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
+import { ref, defineComponent, nextTick, toRaw } from "vue";
+import { countries } from "../../API";
+import { IonSearchbar, IonPage, IonContent } from "@ionic/vue";
+import { GoogleMap } from "@capacitor/google-maps";
+import apiKey from "@/components/APIKey.js";
+
 export default defineComponent({
-  components: {},
+  components: { IonSearchbar, IonPage, IonContent },
   data() {
     return {
       path: this.$route.path === "/home",
       searchQuery: "",
-      countries: countries(),
-      filteredCountries: ref(""), // if proxy import toRaw from vue and use toRaw(filteredCountries[0])
+      searchResult: "",
+      countriesArr: countries(),
+      mapRef: ref<HTMLElement>(),
+      newMap: GoogleMap,
+      newZoom: null as any,
+      newCoordinates: null as any,
     };
   },
   methods: {
+    async createMap() {
+      if (!this.$refs.mapRef) return;
+      this.newMap = await GoogleMap.create({
+        id: "Travel-Map",
+        element: this.$refs.mapRef,
+        apiKey: apiKey.mapsKey,
+        config: {
+          center: this.newCoordinates || {
+            lat: 30,
+
+            lng: 0,
+          },
+          zoom: this.newZoom || 3,
+        },
+      });
+    },
     handleSearch(arr: any) {
-      this.filteredCountries = arr.filter((country: any) =>
-        country.name.toLowerCase().includes(this.searchQuery.toLowerCase())
-      );
+      this.searchResult = arr.filter((country: any) => {
+        return this.searchQuery.toLowerCase() === country.name.toLowerCase();
+      });
+    },
+
+    async resetMap(result: any) {
+      console.log(toRaw(result.coordinates));
+      if (result) {
+        this.newCoordinates = toRaw(result.coordinates);
+        this.newZoom = 6;
+        if (this.newMap) {
+          await this.newMap.removeAllMapListeners();
+          await this.newMap.destroy();
+        }
+        await this.createMap();
+      }
     },
   },
   watch: {
@@ -50,89 +81,12 @@ export default defineComponent({
       this.path = to.path === "/home";
     },
   },
+  mounted() {
+    nextTick(() => {
+      this.createMap();
+    });
+  },
 });
-</script>
-
-<script setup lang="ts">
-import { ref, defineComponent } from "vue";
-import { countries } from "../../API";
-import MyMap from "@/components/MyMap.vue";
-import MarkerInfoWindow from "@/components/MarkerInfoWindow.vue";
-import { Capacitor } from "@capacitor/core";
-
-import {
-  IonSearchbar,
-  IonPage,
-  IonContent,
-  modalController,
-  IonPopover,
-} from "@ionic/vue";
-
-const countriesArr = countries();
-
-const props = defineProps({ mapSearch: Object });
-
-const markerInfo = ref<any>();
-const markerIsOpen = ref<boolean>(false);
-
-// sample data for the map
-const markerData = [
-  // {
-  //   coordinate: { lat: 37.769, lng: -122.446 },
-  //   title: "title one",
-  //   snippet: "title one snippet content will be presented here",
-  // },
-  // {
-  //   coordinate: { lat: 37.774, lng: -122.434 },
-  //   title: "title two",
-  //   snippet: "title two snippet content will be presented here",
-  // },
-  // {
-  //   coordinate: { lat: 37.783, lng: -122.408 },
-  //   title: "title three",
-  //   snippet: "title three snippet content will be presented here",
-  // },
-  // Add more points as needed
-];
-
-const openModal = async (marker: any) => {
-  const modal = await modalController.create({
-    component: MarkerInfoWindow,
-    componentProps: {
-      marker,
-    },
-    initialBreakpoint: 0.2,
-    breakpoints: [0, 0.2],
-    backdropBreakpoint: 0,
-    showBackdrop: false,
-    backdropDismiss: true,
-  });
-
-  modal.present();
-
-  const { data, role } = await modal.onWillDismiss();
-};
-
-const mapClicked = () => {
-  console.log("mapClicked");
-};
-
-const getMarkerInfo = (marker: { latitude: number; longitude: number }) => {
-  return markerData.filter(
-    (m) =>
-      m.coordinate.lat === marker.latitude &&
-      m.coordinate.lng === marker.longitude
-  )[0];
-};
-
-const markerClicked = (event: any) => {
-  console.log(event);
-
-  // only use dialog in web since we doesnt show info window
-  if (!Capacitor.isNativePlatform()) {
-    openModal(getMarkerInfo(event));
-  }
-};
 </script>
 
 <style scoped>
