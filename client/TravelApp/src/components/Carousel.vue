ef126cfe55e7b5a80efe496b95d7744b6f9d5029
Dean <dean96@live.co.uk>
2023-11-14 15:22:29 +0000

    rendered carousel on country page

--- client/TravelApp/src/components/Carousel.vue
+++ client/TravelApp/src/components/Carousel.vue
@@ -1,9 +1,9 @@
 <template>
-	<ion-page>
+	<ion-page class="carouselContainer">
 		<ion-content>
 			<swiper class="carousel">
 				<swiper-slide v-for="pic in pics">
-					<img :src="pic" />
+					<img class="carouselPic" :src="pic" />
 				</swiper-slide>
 			</swiper>
 		</ion-content>
@@ -33,7 +33,7 @@
 	});
 </script>
 
-<style>
+<style scoped>
 	.carousel {
 		height: 100%;
 	}
